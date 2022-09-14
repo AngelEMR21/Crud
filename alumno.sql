478ae3a66c56bc1af42270e7fba38cbc616b8de6
AngelEMR21 <80926784+AngelEMR21@users.noreply.github.com>
2022-09-14 17:36:30 -0500

    Update alumno.sql

--- alumno.sql
+++ alumno.sql
@@ -1,6 +1,11 @@
+
+drop database if exists `cruddb`;
+
 create database cruddb;
 
 use cruddb;
+
+drop table if exists `alumno`; 
 
 CREATE TABLE `alumno` (
   `cod_estudiante` int(11) NOT NULL,
@@ -14,4 +19,3 @@ INSERT INTO `alumno` (`cod_estudiante`, `dni`, `nombres`, `apellidos`) VALUES
 (103, 75854551, 'Saul', 'Segura'),
 (105, 75854553, 'Angel', 'Martinez');
 
-select * from alumno
