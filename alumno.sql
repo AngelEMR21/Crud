6047018cf25de2c0a5092b244526b4fd12e1efca
AngelEMR21 <80926784+AngelEMR21@users.noreply.github.com>
2022-09-13 20:12:20 -0500

    Proyecto CRUD Despliegue

---
+++ alumno.sql
@@ -0,0 +1,17 @@
+create database cruddb;
+
+use cruddb;
+
+CREATE TABLE `alumno` (
+  `cod_estudiante` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `nombres` varchar(20) NOT NULL,
+  `apellidos` varchar(30) NOT NULL
+);
+
+INSERT INTO `alumno` (`cod_estudiante`, `dni`, `nombres`, `apellidos`) VALUES
+(101, 75854549, 'Fernano', 'Orozco'),
+(103, 75854551, 'Saul', 'Segura'),
+(105, 75854553, 'Angel', 'Martinez');
+
+select * from alumno
